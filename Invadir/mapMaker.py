4bafaf829c33d5c9d7a1ea1c0985fe0a43696dbf
seishi1 <major_seishi@hotmail.com>
2018-06-11 22:47:10 -0400

    mapMaker

---
+++ Invadir/mapMaker.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+#logica del creador de mapas
+#solo existen 2 tipos de datos: zonas y vecinos
+#las zonas pueden tener hasta 8 vecinos
+#la cantidad de vecinos sera definido al azar para c/zona
+
+import random
+totalZonas=40
+zonas=[]
+for i in range(totalZonas):
+	zonas.append("zona "+str(i))
+#seleccion de vecinos
+maxVecinos=8
+vecinos=[]
+print(vecinos)
+for i in range(totalZonas):
+	vecinos.append([])
+	for j in range(maxVecinos):
+		vecinos[i].append([])
+		vecinos[i][j].append(random.randint(1,totalZonas))
+print(vecinos)
+#print(totalZonas,zonas)
